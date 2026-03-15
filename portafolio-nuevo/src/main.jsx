4b4d2c669de71daa60b85ef250b82703794da9b0
KeslyUA <kesly2805@gmail.com>
2026-03-14 19:34:32 -0500

    cambio

--- portafolio-nuevo/src/main.jsx
+++ portafolio-nuevo/src/main.jsx
@@ -2,13 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles/fonts.css' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename="/PortafolioNuevo">
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </StrictMode>,
+    </HashRouter>
+  </StrictMode>
 )
